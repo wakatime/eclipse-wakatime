f8fda660f5af9069d16030b8b5f5f7c4dbb75a3a
Alan Hamlett <alan.hamlett@gmail.com>
2016-06-15 17:48:11 +0200

    length is a property on arrays

--- com.wakatime.eclipse.plugin/src/com/wakatime/eclipse/plugin/Dependencies.java
+++ com.wakatime.eclipse.plugin/src/com/wakatime/eclipse/plugin/Dependencies.java
@@ -406,7 +406,7 @@ public class Dependencies {
     private static void deleteDirectory(File path) {
         if( path.exists() ) {
             File[] files = path.listFiles();
-            for(int i=0; i<files.length(); i++) {
+            for(int i=0; i<files.length; i++) {
                 if(files[i].isDirectory()) {
                     deleteDirectory(files[i]);
                 }
