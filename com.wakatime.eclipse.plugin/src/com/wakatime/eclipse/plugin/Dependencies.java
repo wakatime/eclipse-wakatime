7f7c67605b3686cb3bb4f49381019a320000433a
Alan Hamlett <alan.hamlett@gmail.com>
2024-01-21 09:20:32 +0100

    Fix support for socks proxies

--- com.wakatime.eclipse.plugin/src/com/wakatime/eclipse/plugin/Dependencies.java
+++ com.wakatime.eclipse.plugin/src/com/wakatime/eclipse/plugin/Dependencies.java
@@ -20,6 +20,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.net.PasswordAuthentication;
@@ -382,7 +384,7 @@ public class Dependencies {
         String proxyConfig = ConfigFile.get("settings", "proxy", false);
         if (proxyConfig != null && !proxyConfig.trim().equals("")) {
             try {
-                URL proxyUrl = new URL(proxyConfig);
+                URI proxyUrl = new URI(proxyConfig);
                 String userInfo = proxyUrl.getUserInfo();
                 if (userInfo != null) {
                     final String user = userInfo.split(":")[0];
@@ -400,7 +402,7 @@ public class Dependencies {
                     System.setProperty("https.proxyPort", Integer.toString(proxyUrl.getPort()));
                 }
 
-            } catch (MalformedURLException e) {
+            } catch (URISyntaxException e) {
                 Logger.error("Proxy string must follow https://user:pass@host:port format: " + proxyConfig);
             }
         }
